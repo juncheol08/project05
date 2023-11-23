7e796fe569d6d192545e6590c706c7be0effcd18
juncheol08 <juncheol08@naver.com>
2023-11-23 17:44:33 +0900

    update pro05

---
+++ src/main/java/com/tsherpa/team35/entity/ChatRoomVO.java
@@ -0,0 +1,23 @@
+package com.tsherpa.team35.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.aop.framework.AopInfrastructureBean;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomVO {
+
+    private Long roomId;
+    private int productId;
+    private String productTable;
+    private String productName = "";
+    private String buyerId;
+    private String buyerName;
+    private String sellerName = "";
+    private int buyerActive;
+    private String regDate;
+
+}
